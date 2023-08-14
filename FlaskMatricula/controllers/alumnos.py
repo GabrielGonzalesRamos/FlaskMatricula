79b51a36e9e29abd24a94d3656257656b154a5b8
Gabriel Gonzales Ramos <jose.gonzales.r@tecsup.edu.pe>
2023-08-13 23:48:12 -0500

    Añadiendo sentencias SQLAlchemy, Evitando sentencias FLASK-SQLAlchemy

--- FlaskMatricula/controllers/alumnos.py
+++ FlaskMatricula/controllers/alumnos.py
@@ -8,19 +8,29 @@ import copy
 class AlumnosController(Resource):
 
     def get(self):
-        alumnos = AlumnoModel.query.all()
-        lista_alumnos = []
-        for i in alumnos:
-            lista_alumnos.append(i.json())
-        return {
-            'success': True,
-            'message': 'Cantidad total de alumnos {}'.format(AlumnoModel.query.count()),
-            'content': lista_alumnos
-        }, 200
+        alumnos = base_de_datos.session.query(AlumnoModel).all()
+        if alumnos:
+            return {
+                'success': True,
+                'message': 'Cantidad total de alumnos {}'.format(base_de_datos.session.query(AlumnoModel).count()),
+                'content': [i.json() for i in alumnos]
+                }, 200
+        else:
+            return {
+                'success': False,
+                'message': 'No se encontraron alumnos',
+                'content': None
+            }
         
     def post(self):
         data = serializerAlumnos.parse_args()
-        nuevoAlumno = AlumnoModel(nombre=data.get('nombre'), apellido=data.get('apellido'), direccion=data.get('direccion'), pais=data.get('pais'), fecha_nacimiento=data.get('fecha_nacimiento'), matricula=uuid4())
+        nuevoAlumno = AlumnoModel(\
+            nombre=data.get('nombre'), \
+            apellido=data.get('apellido'), \
+            direccion=data.get('direccion'), \
+            pais=data.get('pais'), \
+            fecha_nacimiento=data.get('fecha_nacimiento'), \
+            matricula=uuid4())
         try:
             nuevoAlumno.save()
             return {
@@ -38,24 +48,26 @@ class AlumnosController(Resource):
 class AlumnoController(Resource):
 
     def get(self, id):
-        alumno = AlumnoModel.query.filter_by(alumnoId=id).first()
-        return (
-            {
-            'success': True,
-            'content': alumno.json(),
-            'message': 'Alumno {} {} matriculado'.format(AlumnoModel.query.filter_by(alumnoId=id).first().alumnoNombre, AlumnoModel.query.filter_by(alumnoId=id).first().alumnoApellido)
-            }, 200) if alumno else (
-            {
+        alumno = base_de_datos.session.query(AlumnoModel).filter_by(alumnoId=id).first()
+        if alumno:
+            return {
+                'success': True,
+                'content': alumno.json(),
+                'message': 'Alumno {} {} matriculado'.format(alumno.alumnoNombre, alumno.alumnoApellido)
+                }, 200
+        else:
+            return {
                 'success': False,
                 'content': None,
                 'message': 'Alumno no matriculado'
-            }, 404)
+                }, 404
 
     def put(self, id):
-        alumno = AlumnoModel.query.filter_by(alumnoId=id).first()
+
+        alumno = base_de_datos.session.query(AlumnoModel).filter_by(alumnoId=id).first()
         if alumno:
-            alumno_viejo = copy.deepcopy(alumno)
             data = serializerAlumnos.parse_args()
+            alumno_viejo = copy.deepcopy(alumno)
             alumno.alumnoNombre = data.get('nombre')
             alumno.alumnoApellido = data.get('apellido')
             alumno.alumnoDireccion = data.get('direccion')
@@ -75,7 +87,8 @@ class AlumnoController(Resource):
             }, 404
 
     def delete(self, id):
-        alumno = AlumnoModel.query.filter_by(alumnoId=id)
+
+        alumno = base_de_datos.session.query(AlumnoModel).filter_by(alumnoId=id)
         if alumno.first():
             alumno_viejo = copy.deepcopy(alumno.first())
             alumno.first().delete()
@@ -92,11 +105,13 @@ class AlumnoController(Resource):
             }, 404
 
 class BusquedaAlumnos(Resource):
+
     def get(self):
+
         data = serializerBusqueda.parse_args()
 
         filters = []
-
+        
         if data.get('nombre'):
             filters.append(AlumnoModel.alumnoNombre == data.get('nombre'))
         if data.get('apellido'):
@@ -104,19 +119,19 @@ class BusquedaAlumnos(Resource):
         if data.get('pais'):
             filters.append(AlumnoModel.alumnoPais == data.get('pais'))
 
-        resultado = AlumnoModel.query.filter(*filters).all()
+        resultado = base_de_datos.session.query(AlumnoModel).filter(*filters).all()
 
-        if bool(resultado):
+        if bool(filters):
             return {
                 'success': True,
                 'content': [i.json() for i in resultado],
-                'message': 'Se encontraron {} coincidencias'.format(AlumnoModel.query.filter(*filters).count())
+                'message': 'Se encontraron {} coincidencias'.format(base_de_datos.session.query(AlumnoModel).filter(*filters).count())
             }
         else: 
             return {
                 'success': False,
                 'content': None,
-                'message': 'Se encontraron 0 coincidencias'
+                'message': 'Filtro vacío'
             }
 
 
