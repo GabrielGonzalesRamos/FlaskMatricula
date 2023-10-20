360d651fb6ec3031585888eff6d61472c244ef80
Gabriel Gonzales Ramos <jose.gonzales.r@tecsup.edu.pe>
2023-10-20 14:02:10 -0500

    Modificando el archivo test/unittest/base_test.py; usando únicamente el módulo OS

--- test/unit/base_test.py
+++ test/unit/base_test.py
@@ -3,16 +3,18 @@ from unittest import TestCase
 from app import app 
 from config.conexion_bd import base_de_datos
 
+print(os.getenv('GITHUB_ENV'))
+
 class BaseTest(TestCase):
     @classmethod
     def setUpClass(cls):
-        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI_DEV')
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI_DEV')
         base_de_datos.create_all(app=app)
 
     @classmethod
     def tearDownClass(cls):
         base_de_datos.drop_all(app=app)
-        
+
     def setUp(self):
         self.app = app.test_client
         self.app_context = app.app_context
